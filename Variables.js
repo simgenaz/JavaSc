f3541bccde542e09a34eca854579443b9366ed5e
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-07-11 16:24:52 +0300

    first commit

---
+++ Variables.js
@@ -0,0 +1,59 @@
+let x = 5;
+// x değerine 5 i atıyoruz
+let y = 6;
+// y değerine 6 yı atıyoruz
+let z = x + y;
+// burda z değişkeni tanımlayıp çıkan sonucu ona aktarıyoruz
+
+// Farklı değişkenler de mevcuttur. Değişkenlerin aralarında farklar vardır.
+// bunları detaylı incelicez.
+
+var a = 5;
+var b = 8;
+var c = a * b;
+
+let v = 1;
+let r = 7;
+let l = x - y;
+
+const a = 9;
+const b = 8;
+const c = a / b;
+
+
+const price1 = 15;
+const price2 = 30;
+let total = price1 + price2;
+
+// price1 ve price2 const değişkeniyle tanımlandığı için değişemez fakat total let ile tanımlandığı için değişebilir.
+
+
+let simgeYas = 25;
+let aliYas = 35;
+const yasFarki = aliYas - simgeYas;
+ //simgenin ve alinin yaşını letle belirledik çünkü yaşları değişkenlik gösterebilir
+ // fakat aralarındaki yaş farkı değişmiceği için constla belirledik.
+
+var klavyeIsmi ;
+let klavyeIsmi ; 
+ // var ve let değişkenleriyle tanımlama yapabiliriz.
+
+var klavyeIsmi = "Asus";
+
+klavyeIsmi = "Asus";
+// şeklinde tanımlama yapabiliriz.
+
+// bir ifade de birden fazla tanımlama yapabiliriz mesela
+
+let person = "Simgenaz" , klavyeIsmi = "Asus" , yas = 25;
+
+//let veya const ile bildirilen bir değişkeni tekrardan tanımlanmaz.
+
+let s = 5 + 8 + 13; //bu şekilde toplama işlemi yapabiliriz 
+
+let aa = "Simge" + " " + "Naz"; //Stringleri + işaretiyle ayırabiliriz.
+
+let $ = "Hello World";
+let $$ = 15;
+// dolar işareti çok fazla kullanılan bir kütüphane değildir.
+// Onun yerine Html elementlerini seçmek için kullanıyoruz ve $("p") mesela bu bütün p elementlerini seç demektir.
