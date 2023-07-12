ce5c7caea50d6ca8e3012ccd5af46e995aa55480
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-07-12 14:39:10 +0300

    numbers

---
+++ Numbers.js
@@ -0,0 +1,43 @@
+//Numbers
+
+let x = 3; //ondalık sayısız
+let y = 3.14; //ondalık sayılı
+
+
+//Numaralar arasında işlemler, tanımla vs yapılabilir
+
+let a = 3; //numara
+let b = "3"; //Stringdir
+
+//Numara methodlarında kullandıklarımız 
+// toString()	Numarayı string şekilde döndürür
+// toExponential()	üstel gösterimde bir sayı döndürür
+// toFixed()	Virgüllü bir sayı döndürür
+// toPrecision()	Sabit bir uzunlukta sayıyı döndürür
+// ValueOf()	Sayıyı sayı olarak döndürür
+
+// EPSILON	1 ve en küçük sayı arasındaki farkı bulur
+// MAX_VALUE	en büyük sayıyı döndürür
+// MIN_VALUE	en küçük sayıyı döndürür
+// MAX_SAFE_INTEGER	The maximum safe integer (253 - 1)
+// MIN_SAFE_INTEGER	The minimum safe integer -(253 - 1)
+// POSITIVE_INFINITY	Infinity (returned on overflow)
+// NEGATIVE_INFINITY	Negative infinity (returned on overflow)
+// NaN	A "Not-a-Number" value
+
+let x2 = Number.EPSILON;
+let x3 = Number.MAX_VALUE;
+let x4 = 6;
+x4.MAX_VALUE
+
+let x5 = Number.MIN_VALUE;
+
+let x6 = Number.MAX_SAFE_INTEGER;
+
+let x7 = Number.MIN_SAFE_INTEGER;
+
+let x8 = Number.POSITIVE_INFINITY;
+
+let x9 = Number.NEGATIVE_INFINITY;
+
+let xx = Number.NaN;
