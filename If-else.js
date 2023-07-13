e5b2466ae83648a446c0fa74bb633db44e390869
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-07-13 12:52:40 +0300

    If-else

---
+++ If-else.js
@@ -0,0 +1,9 @@
+//If else durumları hangi sorgu doğruysa o sorgunun içindekini döndürür
+
+if (condition1) {
+    //condition1 doğruysa burası döner
+  } else if (condition2) {
+   //condition2 doğruysa burası döner
+  } else {
+    //  hiç biri doğru değilse burası döner
+  }
