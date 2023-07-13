40d7c2b12c735ff250d5159eaf07b85592ce91ce
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-07-13 12:34:03 +0300

    date

---
+++ Date.js
@@ -0,0 +1,16 @@
+const d = new Date();
+ 
+new Date()
+//new Date(date string)
+
+new Date(year,month)
+new Date(year,month,day)
+new Date(year,month,day,hours)
+new Date(year,month,day,hours,minutes)
+new Date(year,month,day,hours,minutes,seconds)
+new Date(year,month,day,hours,minutes,seconds,ms)
+
+new Date(milliseconds)
+
+//date methodlarını çekerken önüne get methodu koyup yapabiliriz
+//eğer date yani tarihte bir şey değiştiriceksek set methoduyla bunu yapabiliriz.
