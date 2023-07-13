5a720907cd7cebf696bc43559df6f3621adec3df
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-07-13 12:42:02 +0300

    Matematik işlemleri

---
+++ math.js
@@ -0,0 +1,17 @@
+//Math methodları genellikle işlemleri yaparken daha kolaylık sağlamak için bulunur
+Math.E        
+Math.PI       
+Math.SQRT2    
+Math.SQRT1_2  
+Math.LN2      
+Math.LN10     
+Math.LOG2E    
+Math.LOG10E   
+
+Math.round(4.6); // en yakın sayıyı döndürür
+Math.ceil(4.9);  //en yakın üst sayıyı döndürür
+Math.floor(4.9); //en yakın alt sayıyı döndürür
+Math.trunc(4.9); //noktadan önceki sayıyı döndürür
+Math.pow(8, 2); // x üzeri y yi alır
+Math.random(); //random sayı döndürür
+
